1a1507a848dcbb6dd9e210762f9571e41edf20aa
MuiMui6 <b6340@oic.jp>
2018-12-16 00:19:10 +0900

    ルートの追加

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,139 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//全ユーザ共通
+    //トップ
+    Route::get('/', function () {
+        return view('home');
+    });
+
+    //商品詳細
+    Route::get('/Detail_Item', function () {
+        return view('Deteil_Item');
+    });
+
+    //観光地一覧
+    Route::get('/All_Spot', function () {
+        return view('All_Spot');
+    });
+
+    //観光地詳細
+    Route::get('/Detail_Spot', function () {
+        return view('Detail_Spot');
+    });
+
+    //住所一覧
+    Route::get('/All_Address', function () {
+        return view('All_Address');
+    });
+
+    //カート
+    Route::get('/Register_Cart', function () {
+        return view('Register_Cart');
+    });
+
+    //カート確認
+    Route::get('/Confirmor_Cart', function () {
+        return view('Confirmor_Cart');
+    });
+
+    //カート履歴
+    Route::get('/History_Cart', function () {
+        return view('History_Cart');
+    });
+
+//管理者のみ
+//一覧系
+    //住所一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //商品一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //商品コメント一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //受注一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //ピース一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //サイズ一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //観光地一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //観光地コメント一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //タグ一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+    //ユーザ一覧
+    Route::get('/', function () {
+        return view('admin.');
+    });
+
+//編集
+    //住所編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //商品編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //商品コメント編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //受注編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //観光地編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //観光地コメント編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+    //ユーザ編集
+    Route::get('/', function () {
+        return view('admin.Edit_');
+    });
+
+
+
+
+
+
