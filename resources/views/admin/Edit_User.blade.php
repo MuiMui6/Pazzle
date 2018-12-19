c99f1c4221307e03c65b2aa5c3f4c05d8595302d
MuiMui6 <b6340@oic.jp>
2018-12-20 00:52:02 +0900

    Edit商品一部・ユーザ画面仮完成

--- resources/views/admin/Edit_User.blade.php
+++ resources/views/admin/Edit_User.blade.php
@@ -4,29 +4,44 @@
     <div class="row">
 
         <div class="col-12 m-3">
-            <h3 class="text-center">Edit Spot</h3>
+            <h3 class="text-center">Edit User</h3>
         </div>
         <div class="m-3">
-            <h5>観光地に関する情報を作成・編集できます。</h5>
+            <h5>ユーザに関する情報を編集できます。</h5>
         </div>
 
 
         <table class="table table-borderless">
             <tbody>
             <tr>
-                <th class="text-center"></th>
-                <th class="text-center"></th>
+                <th class="text-center">Name</th>
+                <th class="text-center">
+                    <input type="text" class="form-control">
+                </th>
             </tr>
             <tr>
-                <th class="text-center"></th>
-                <th class="text-center"></th>
+                <th class="text-center">Email</th>
+                <th class="text-center">
+                    <input type="email" class="form-control">
+                </th>
             </tr>
             <tr>
-                <th class="text-center"></th>
-                <th class="text-center"></th>
+                <th class="text-center">Rank</th>
+                <th class="text-center">
+                    <input type="radio"><span class="m-3 mr-5">Admin</span>
+                    <input type="radio"><span class="m-3">User</span>
+                </th>
+            </tr>
+            <tr>
+                <th class="text-center">Etc</th>
+                <th class="text-center">
+                    <textarea rows="5" cols="50" class="form-control"></textarea>
+                </th>
             </tr>
             </tbody>
         </table>
+
+        <input type="submit" value="保存" class="btn btn-block">
 
     </div>
 @endsection
