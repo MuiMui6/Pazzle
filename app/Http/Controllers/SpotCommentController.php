3ef1823f6bee7ec0e2d71b3acafbe6fe63edf1da
MuiMui6 <b6340@oic.jp>
2018-12-20 20:34:04 +0900

    テンプレ貼り付け

--- app/Http/Controllers/SpotCommentController.php
+++ app/Http/Controllers/SpotCommentController.php
@@ -7,17 +7,67 @@ use Illuminate\Http\Request;
 class SpotCommentController extends Controller
 {
 //表示
+//一般ユーザ
+    public function user_view()
+    {
+
+    }
+
+//管理者側
+    public function admin_view()
+    {
+
+    }
 
 
 //検索
+//一般ユーザ
+    public function user_serch()
+    {
 
+    }
+
+//管理者側
+    public function admin_search()
+    {
+
+    }
 
 //詳細
+//一般ユーザ
+    public function user_detail()
+    {
 
+    }
+
+//管理者側
+    public function admin_detail()
+    {
+
+    }
 
 //追加
+    public function user_add()
+    {
 
+    }
+
+//管理者側
+    public function admin_add()
+    {
+
+    }
 
 //編集
+    public function user_edit()
+    {
+
+    }
+
+//管理者側
+    public function admin_edit()
+    {
+
+    }
 
 }
