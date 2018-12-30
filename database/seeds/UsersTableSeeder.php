edb3ea4b673e2faf8d05663108bf735ca72a4fa3
MuiMui6 <b6340@oic.jp>
2018-12-30 13:45:18 +0900

    テストの作成、訂正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,7 +23,7 @@ class UsersTableSeeder extends Seeder
 
 
         //一般ユーザ
-        for($i = 0; $i < 100; $i++) {
+        for($i = 1; $i < 80; $i++) {
             User::create([
                 'name' => 'user'.$i,
                 'email' => 'mail'.$i.'@mail.com',
