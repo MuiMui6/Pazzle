a0287b8dd01489682ba6418c4572a7ebf8ccda14
MuiMui6 <b6340@oic.jp>
2019-02-05 22:19:20 +0900

    user1に管理者権限追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,27 +12,37 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+
+        User::create([
+            'name' => 'user1',
+            'email' => 'mail1@mail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('secret'),
+            'anser' => '1234567890',
+            'rank' => '1'
+        ]);
+
+        //一般ユーザ
+        for ($i = 2; $i < 480; $i++) {
+            User::create([
+                'name' => 'user' . $i,
+                'email' => 'mail' . $i . '@mail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('secret'),
+                'anser' => '1234567890',
+                'rank' => '0'
+            ]);
+        }
+
+
         //管理者
         User::create([
             'name' => 'admin',
             'email' => 'admin@mail.com',
             'email_verified_at' => now(),
             'password' => bcrypt('secret'),
-            'anser'=>'1234567890',
+            'anser' => '1234567890',
             'rank' => '1'
         ]);
-
-
-        //一般ユーザ
-        for($i = 1; $i < 480; $i++) {
-            User::create([
-                'name' => 'user'.$i,
-                'email' => 'mail'.$i.'@mail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('secret'),
-                'anser'=>'1234567890',
-                'rank' => '0'
-            ]);
-        }
     }
 }
