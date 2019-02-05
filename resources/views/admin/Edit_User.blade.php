d5d2a65ecd7441a87e9ac702cbadfa4591ec58f9
MuiMui6 <b6340@oic.jp>
2019-02-05 21:45:54 +0900

    管理者側ユーザ管理を編集（日本語・英語対応）

--- resources/views/admin/Edit_User.blade.php
+++ resources/views/admin/Edit_User.blade.php
@@ -7,9 +7,11 @@
 
                 <div class="col-12 m-3">
                     <h3 class="text-center">Edit User</h3>
+                    <p class="text-center">ユーザ編集</p>
                 </div>
                 <div class="m-3">
-                    <h5>ユーザに関する情報を編集できます。</h5>
+                    <h5>I can edit the information about the user.</h5>
+                    <p>ユーザに関する情報を編集できます</p>
                 </div>
 
                 <form action="/admin/Edit_User/Detail" method="post">
@@ -20,47 +22,74 @@
                             <input type="hidden" value="{{Auth::user()->id}}" name="userid">
                             <input type="hidden" value="{{$user->id}}" name="id">
                             <tr>
-                                <th class="text-center">Name</th>
+                                <th class="text-center">
+                                    <h5>User ID</h5>
+                                    <p>ユーザID</p>
+                                </th>
+                                <th class="text-center">
+                                    {{$user->id}}
+                                </th>
+                            </tr>
+                            <tr>
+                                <th class="text-center">
+                                    <h5>User Name</h5>
+                                    <p>ユーザ名</p>
+                                </th>
                                 <th class="text-center">
                                     <input type="text" class="form-control" placeholder="{{$user->name}}" name="name">
                                 </th>
                             </tr>
                             <tr>
-                                <th class="text-center">Email</th>
+                                <th class="text-center">
+                                    <h5>E-Mail</h5>
+                                    <p>Eメール</p>
+                                </th>
                                 <th class="text-center">
                                     <input type="email" class="form-control" placeholder="{{$user->email}}"
                                            name="email">
                                 </th>
                             </tr>
                             <tr>
-                                <th class="text-center">Rank</th>
+                                <th class="text-center">
+                                    <h5>Rank</h5>
+                                    <p>ランク</p>
+                                </th>
                                 <th class="text-center">
                                     @switch($user->rank)
                                         @case('0')
-                                        <input type="radio" value="0" name="rank" checked><span class="m-3 mr-5">User</span>
+                                        <input type="radio" value="0" name="rank" checked><span
+                                                class="m-3 mr-5">User</span>
                                         <input type="radio" value="1" name="rank"><span class="m-3 mr-5">Admin</span>
                                         <input type="radio" value="2" name="rank"><span class="m-3">Stop User</span>
                                         @break
                                         @case('1')
                                         <input type="radio" value="0" name="rank"><span class="m-3 mr-5">User</span>
-                                        <input type="radio" value="1" name="rank"checked><span class="m-3 mr-5">Admin</span>
+                                        <input type="radio" value="1" name="rank" checked><span
+                                                class="m-3 mr-5">Admin</span>
                                         <input type="radio" value="2" name="rank"><span class="m-3">Stop User</span>
                                         @break
                                         @default
                                         <input type="radio" value="0" name="rank"><span class="m-3 mr-5">User</span>
                                         <input type="radio" value="1" name="rank"><span class="m-3 mr-5">Admin</span>
-                                        <input type="radio" value="2" name="rank" checked><span class="m-3">Stop User</span>
-                                        @endswitch
+                                        <input type="radio" value="2" name="rank" checked><span
+                                                class="m-3">Stop User</span>
+                                    @endswitch
                                 </th>
                             </tr>
                             <tr>
-                                <th class="text-center">CreateDate</th>
+                                <th class="text-center">
+                                    <h5>Created Date</h5>
+                                    <p>作成日</p>
+                                </th>
                                 <th class="text-center">
                                     {{$user->created_at->format('Y年m月d日')}}
                                 </th>
                             </tr>
                             <tr>
-                                <th class="text-center">UpdateDate</th>
+                                <th class="text-center">
+                                    <h5>Updated Date</h5>
+                                    <p>更新日</p>
+                                </th>
                                 <th class="text-center">
                                     {{$user->updated_at->format('Y年m月d日')}}
                                 </th>
@@ -68,7 +97,7 @@
                         @endforeach
                         </tbody>
                     </table>
-                    <button type="submit" class="btn btn-block btn-primary">保存</button>
+                    <button type="submit" class="btn btn-block btn-primary">Save　/　保存</button>
 
                 </form>
 
